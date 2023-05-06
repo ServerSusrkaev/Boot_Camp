3a86c559b355710ebde61e77e1ab897a1aebf6cb
Server Susurkaev <susurkaev@yandex.ru>
2023-05-06 09:11:04 +0300

    добавлен код подсчета одиноковых элементов в массиве и их сортировка

---
+++ Сортировка подсчетом/Program.cs
@@ -0,0 +1,35 @@
+﻿//Сортировка методом подсчета
+
+int[] array = { 2, 1, 2, 3, 2, 3 };
+int[] count = new int[array.Length];
+
+//Определение количества повторяющихся элементов в исходном массиве array[]
+// внесение их во вспомогателный массив caount[]
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+    count[array[i]]++;
+}
+Console.WriteLine("");
+for (int i = 0; i < count.Length; i++)
+{
+    Console.Write($"{count[i]} ");
+}
+
+//сортировака отсортированных элементов в исходном массиве 
+Console.WriteLine("");
+int k = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    for (int j = 0; j < count[i]; j++)
+    {
+        array[k] = i;
+        k++;
+    }
+}
+
+//вывод отсортированых элементов
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
